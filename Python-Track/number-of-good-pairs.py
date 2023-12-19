8c50e29bab8f673385001886221d70236056e55a
Ephrem G <84809084+ephyg@users.noreply.github.com>
2023-12-19 23:55:13 +0300

    Added solution for Number of Good Pairs

---
+++ Python-Track/number-of-good-pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        dic=Counter(nums)
+        ans=0
+        ls=dic.keys()
+        for i in ls:
+            if(dic[i]>1):
+                a=factorial(dic[i])
+                b=factorial(dic[i]-2)
+                c=a/(b*2)
+                ans+=c
+        return int(ans)
