8642e899081bbbd0da8c5c11472df45b34c07810
Ephrem G <84809084+ephyg@users.noreply.github.com>
2023-12-22 11:21:34 +0300

    Added solution for Replace Elements in an Array

---
+++ Python-Track/replace-elements-in-an-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        dict={}
+        for i in range(len(nums)):
+            dict[nums[i]]=i
+        
+        for i in range(len(operations)):
+            nums[dict[operations[i][0]]] = operations[i][1]
+            dict[operations[i][1]]=dict[operations[i][0]]
+            dict.pop(operations[i][0])
+            
+        return nums
