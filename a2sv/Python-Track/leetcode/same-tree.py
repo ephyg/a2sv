7cb0808f4b96222b92e0e2dca743341e527e321b
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-02-27 14:53:29 +0300

    Add solution for Same Tree

--- a2sv/Python-Track/leetcode/same-tree.py
+++ a2sv/Python-Track/leetcode/same-tree.py
@@ -1,10 +1,16 @@
-class Solution(object):
-    def isSameTree(self, p, q):
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-        if p.val != q.val:
-            return False
-        
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def same(p,q):
+            if p and q:
+                if q.val==p.val:
+                    return same(p.left,q.left) and same(p.right,q.right)
+                return False
+            if not q and not p:
+                return True
+        return same(p,q)
