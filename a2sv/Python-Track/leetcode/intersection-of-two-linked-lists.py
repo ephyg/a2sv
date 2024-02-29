0a5393c4228cd6ba6cbdc5a24fa3c3062368679e
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-02-29 08:29:17 +0300

    Add solution for Intersection of Two Linked Lists

--- a2sv/Python-Track/leetcode/intersection-of-two-linked-lists.py
+++ a2sv/Python-Track/leetcode/intersection-of-two-linked-lists.py
@@ -6,21 +6,26 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        node_a=headA
-        node_b=headB
-        dct=set()
-        if not node_a or not node_b  :
-            return
-        if node_a==node_b:
-            return node_a
-        while node_a and node_a.next:
-            dct.add(node_a)
-            node_a=node_a.next
-        while node_b and node_b.next:
-            if node_b in dct:
-                return node_b
-            node_b=node_b.next
-        if node_a==node_b:
-            return node_a
-
-
+        A=0
+        B=0
+        hA=headA
+        hB=headB
+        while hA:
+            A+=1
+            hA=hA.next
+        while hB:
+            B+=1
+            hB=hB.next
+        if A>B:
+            for i in range(A-B):
+                headA=headA.next
+        else:
+            for i in range(B-A):
+                headB=headB.next
+        
+        while headA:
+            if headA==headB:
+                return headA
+            headA=headA.next
+            headB=headB.next
+        return None
