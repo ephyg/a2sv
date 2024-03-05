0b042ab05d6c0f1fb44704935d72fd6f27de40ee
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-03-05 13:53:31 +0300

    Add solution for Combination Sum II

---
+++ a2sv/Python-Track/leetcode/combination-sum-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = set()
+        def combine(idx, arr, _sum):
+            if _sum == target:
+                ans.add(tuple(sorted(arr[:])))
+                return
+
+            if _sum > target:
+                return
+                
+            visited = set()
+            for i in range(idx, len(candidates)):
+                if not candidates[i] in visited:
+                    arr.append(candidates[i])
+                    _sum += candidates[i]
+                    combine(i + 1, arr, _sum)
+                    visited.add(candidates[i])
+                    _sum -= candidates[i]
+                    arr.pop()
+        combine(0, [], 0)
+        return ans
