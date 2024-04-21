17bddc27dc898486b0e50737ff04769c04539718
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-04-21 22:50:59 +0300

    Created 'Valid parenthesis' by A2SV Hub

---
+++ 21-Apr-2024/Valid parenthesis 129544.py
@@ -0,0 +1,16 @@
+# Problem: Valid parenthesis - https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        my_dict={"(":")","[":"]","{":"}"}
+        stack=[]
+        for i in range(len(s)):
+            if s[i] in my_dict:
+                stack.append(s[i])
+            else:
+                if not stack:
+                    return False
+                x=stack.pop()
+                if my_dict[x]!=s[i]:
+                    return False
+        return len(stack)==0
