eff5813578fe8a0d728519994e28b2d2d1c716c0
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-05-02 20:07:18 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 02-May-2024/Fedor and New Game 139229.py
@@ -0,0 +1,28 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+import sys
+from collections import defaultdict,Counter,deque
+from math import ceil,gcd
+n,m,k=list(map(int, sys.stdin.readline().strip().split()))
+nums=[]
+for i in range(m+1):
+    num=int(sys.stdin.readline().strip())
+    nums.append(num)
+    # print(bin(num)[2:])
+ans=0
+last=nums[-1]
+for i in range(m):
+    count=0
+    temp=last
+    while temp | nums[i]:
+        if temp%2!=nums[i]%2:
+            count+=1
+        temp>>=1
+        nums[i]>>=1
+    if count<=k:
+        ans+=1
+print(ans)
+
+
+
+
