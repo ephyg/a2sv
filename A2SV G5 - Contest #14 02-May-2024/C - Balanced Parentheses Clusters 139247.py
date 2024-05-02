c178cb8d91eb9062858d9ee013e5c4125c732176
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-05-02 20:32:43 +0300

    Created 'C - Balanced Parentheses Clusters' by A2SV Hub

---
+++ A2SV G5 - Contest #14 02-May-2024/C - Balanced Parentheses Clusters 139247.py
@@ -0,0 +1,24 @@
+# Problem: C - Balanced Parentheses Clusters - https://codeforces.com/gym/520688/problem/C
+
+from collections import Counter, defaultdict
+from math import ceil
+import sys
+def lsinp():
+    return sys.stdin.readline().strip().split()
+def liinp():
+    return list(map(int, sys.stdin.readline().strip().split()))
+def sinp():
+    return sys.stdin.readline().strip()
+def iinp():
+    return int(sys.stdin.readline().strip())
+ 
+
+t=iinp()
+for i in range(t):
+    n=iinp()
+    s=sinp()
+
+    for i in range(1,len(s)):
+        if s[i-1]==")" and s[i]=="(":
+            n-=1
+    print(n)
