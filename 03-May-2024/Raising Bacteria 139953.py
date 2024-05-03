89d806b90af041b1080de81ec484850cc8ac0d46
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-05-03 22:35:09 +0300

    Created 'Raising Bacteria' by A2SV Hub

---
+++ 03-May-2024/Raising Bacteria 139953.py
@@ -0,0 +1,9 @@
+# Problem: Raising Bacteria - https://codeforces.com/contest/579/problem/A
+
+import sys
+from collections import defaultdict,Counter,deque
+from math import ceil,gcd
+n=int(sys.stdin.readline().strip())
+
+count=n.bit_length()
+print(bin(n)[2:].count("1"))
