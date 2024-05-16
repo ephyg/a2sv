035332e273b56b51433d6b659c13ed1c2268c286
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-05-16 23:22:17 +0300

    Created 'C - ANDy Session' by A2SV Hub

---
+++ A2SV G5 - Contest #15 16-May-2024/C - ANDy Session 152679.py
@@ -0,0 +1,23 @@
+# Problem: C - ANDy Session - https://codeforces.com/gym/522079/problem/C
+
+import sys
+from collections import defaultdict, Counter, deque
+from math import ceil, gcd
+t = int(sys.stdin.readline().strip())
+for _ in range(t):
+    n, k = list(map(int, sys.stdin.readline().strip().split()))
+    nums = list(map(int, sys.stdin.readline().strip().split()))
+    bits = [0]*35
+    for i in range(n):
+        temp = bin(nums[i])[2:]
+        x = 35-len(temp)
+        temp = "0"*x+temp
+        for i in range(35):
+            if temp[i] == "0":
+                bits[i] += 1        
+    ans = 0
+    for i in range(4,35):
+        if bits[i] <= k:
+            ans += pow(2, 34-i)
+            k -= bits[i]
+    print(ans)
