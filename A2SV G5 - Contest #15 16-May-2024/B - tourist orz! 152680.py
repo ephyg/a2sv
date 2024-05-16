aeff21be976f2f02e5a2388a8ef683354fd58127
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-05-16 23:22:27 +0300

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 16-May-2024/B - tourist orz! 152680.py
@@ -0,0 +1,19 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+import sys
+from collections import defaultdict,Counter,deque
+from math import ceil,gcd
+t=int(sys.stdin.readline().strip())
+for _ in range(t):
+    n,z=list(map(int, sys.stdin.readline().strip().split()))
+    nums=list(map(int, sys.stdin.readline().strip().split()))
+    max_=float("-inf")
+    for i in range(n):
+        And=z&nums[i]
+        z=max(And,z)
+    for j in range(n):
+        otherOr=z|nums[j]
+        otherAnd=z|nums[j]
+        max_=max(otherOr,otherAnd,max_)
+    print(max_)
+
