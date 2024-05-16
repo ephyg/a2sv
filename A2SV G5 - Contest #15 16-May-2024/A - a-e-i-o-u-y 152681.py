70be20a6ce515501927a55b9e7d6bca0128ac7b2
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-05-16 23:22:43 +0300

    Created 'A - a-e-i-o-u-y' by A2SV Hub

---
+++ A2SV G5 - Contest #15 16-May-2024/A - a-e-i-o-u-y 152681.py
@@ -0,0 +1,19 @@
+# Problem: A - a-e-i-o-u-y - https://codeforces.com/gym/522079/problem/A
+
+import sys
+from collections import defaultdict,Counter,deque
+from math import ceil,gcd
+n=int(sys.stdin.readline().strip())
+s=sys.stdin.readline().strip()
+vowels="aeiouy"
+stack=[]
+for i in range(n):
+    if not stack:
+        stack.append(s[i])
+    else:
+        if stack[-1] in vowels and s[i] in vowels:
+            continue
+        else:
+            stack.append(s[i])
+print("".join(stack))
+        
