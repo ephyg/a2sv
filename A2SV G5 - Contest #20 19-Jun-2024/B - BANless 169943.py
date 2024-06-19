0b93b86fabf349f7f209512c1eb92f83c2b839ab
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-06-19 16:10:33 +0300

    Created 'B - BANless' by A2SV Hub

---
+++ A2SV G5 - Contest #20 19-Jun-2024/B - BANless 169943.py
@@ -0,0 +1,45 @@
+# Problem: B - BANless - https://codeforces.com/gym/528792/problem/B
+
+import sys, threading
+from collections import defaultdict, Counter, deque
+from math import ceil, gcd, sqrt
+from heapq import heapify, heappop, heapreplace, heappush
+from bisect import bisect_right, bisect_left
+
+def lsinp():
+    return sys.stdin.readline().strip().split()
+def liinp():
+    return list(map(int, sys.stdin.readline().strip().split()))
+def sinp():
+    return sys.stdin.readline().strip()
+def iinp():
+    return int(sys.stdin.readline().strip())
+
+def solve():
+    n = iinp()
+    operation=n//2+1
+    result=[]
+    left=1
+    right=n*3
+    while right > left:
+        result.append([left,right])
+        left+=3
+        right-=3
+    print(len(result))
+    for x,y in result:
+        print(x,y)
+t = iinp()
+for _ in range(t):
+    solve()
+
+# def main():
+#     pass
+
+# if __name__ == '__main__':
+
+#     sys.setrecursionlimit(1 << 30)
+#     threading.stack_size(1 << 27)
+
+#     main_thread = threading.Thread(target=main)
+#     main_thread.start()
+#     main_thread.join()
