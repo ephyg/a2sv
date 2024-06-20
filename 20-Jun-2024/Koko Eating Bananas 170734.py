1535d73f1bbafe8bd768f1e5bea839f862bdc823
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-06-20 20:34:44 +0300

    Created 'Koko Eating Bananas' by A2SV Hub

---
+++ 20-Jun-2024/Koko Eating Bananas 170734.py
@@ -0,0 +1,18 @@
+# Problem: Koko Eating Bananas - https://leetcode.com/problems/koko-eating-bananas/
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def validate(k):
+            hour=0
+            for i,num in enumerate(piles):
+                hour+=ceil(num/k)
+            return hour
+        left,right,best=1,max(piles),float("inf")
+        while left<=right:
+            mid=left+(right-left)//2
+            if validate(mid)<=h:
+                best=mid
+                right=mid-1
+            else:
+                left=mid+1
+        return best
