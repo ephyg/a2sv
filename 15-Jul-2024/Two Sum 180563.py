10ebd8ba7b7eecd9e84d6a134882f276b3f80ce7
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-07-15 21:02:59 +0300

    Created 'Two Sum' by A2SV Hub

---
+++ 15-Jul-2024/Two Sum 180563.py
@@ -0,0 +1,18 @@
+# Problem: Two Sum - https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ans=[]
+        check=False
+        for i in range(len(nums)-1):
+            for j in range (i+1,len(nums)):
+                if (nums[i]+nums[j]==target):
+                    ans.append(i)
+                    ans.append(j)
+                    check=True
+                    break
+            if(check):
+                break
+        return ans
+
+
