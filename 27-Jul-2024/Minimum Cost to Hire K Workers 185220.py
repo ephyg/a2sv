8c268492f0cd32f2d0b93f07c4a548bc30921761
Ephrem G <84809084+ephyg@users.noreply.github.com>
2024-07-27 17:51:05 +0300

    Created 'Minimum Cost to Hire K Workers' by A2SV Hub

---
+++ 27-Jul-2024/Minimum Cost to Hire K Workers 185220.py
@@ -0,0 +1,23 @@
+# Problem: Minimum Cost to Hire K Workers - https://leetcode.com/problems/minimum-cost-to-hire-k-workers/description/
+
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        n=len(quality)
+        worker=[[w/q, q] for w, q in zip(wage, quality)]
+
+        worker.sort()
+        quality_sum=0
+        heap=[]
+
+        for ratio, q in worker[:k]:
+            quality_sum+=q
+            heappush(heap, -q)
+        pay=worker[k-1][0]*quality_sum
+
+        for ratio, q in worker[k:n]:
+            heappush(heap, -q)
+            quality_sum+=q+heappop(heap)
+            pay=min(pay, quality_sum*ratio)
+
+        return pay
+        
